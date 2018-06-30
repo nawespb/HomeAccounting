3a933c8bd939e6b3fde9443f5a4236bc7baf2f18
nawe.spb <nawe.spb@gmail.com>
2018-06-30 20:48:24 +0300

    Connector updated

--- src/main/java/app/DBConnector.java
+++ src/main/java/app/DBConnector.java
@@ -6,6 +6,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class DBConnector {
     
@@ -13,8 +15,15 @@ public class DBConnector {
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
     
-    private static final String SELECT = "SELECT ID, PRODUCT_NAME FROM accounting where id = ?;";
-    private static final String INSERT = "INSERT INTO accounting (PRODUCT_GROUP, PRODUCT_NAME, PRICE) VALUES (?, ?, ?);";
+    private static final String SELECT = "SELECT ID, PRODUCT_NAME FROM accounting WHERE ID = ?;";
+    private static final String SELECT_ALL = "SELECT * FROM accounting LIMIT ?;";
+    private static final String SELECT_BY_DATE = "SELECT * FROM accounting "
+            + "WHERE SPENDING_DATE BETWEEN ? AND ?;";
+    private static final String INSERT = "INSERT INTO accounting "
+            + "(SPENDING_DATE, PRODUCT_GROUP, PRODUCT_NAME, PRICE) VALUES (?, ?, ?, ?);";
+    private static final String UPDATE = "UPDATE accounting "
+            + "SET PRODUCT_NAME = ?, PRICE = ? WHERE ID = ?";
+    private static final String DELETE = "DELETE FROM accounting WHERE ID = ?;";
     
     private Connection con;
     private ResultSet rs;
@@ -33,7 +42,9 @@ public class DBConnector {
         
     }
     
-public void executeSelect (int id) {
+    public String select (int id) {
+        
+        String result = "";
         
         try {
             
@@ -41,23 +52,92 @@ public void executeSelect (int id) {
             prep.setInt(1, id);
             rs = prep.executeQuery();
             while (rs.next()) {
-                int count = rs.getInt(1);
+                int pId = rs.getInt(1);
                 String name = rs.getString(2);
-                System.out.println(count + " " + name);
+                result = pId + name;
             }
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+    
+    public ArrayList<String> selectAll (int limit) {
+        
+        ArrayList<String> arr = new ArrayList<>();
+        try {
+            
+            prep = con.prepareStatement(SELECT_ALL);
+            prep.setInt(1, limit);
+            rs = prep.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(4);
+                arr.add("ID: " + id + ": " + name);
+            }
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return arr;
+    }
+    
+    public ArrayList<String> selectByDate (Date from, Date to) {
+        
+        ArrayList<String> arr = new ArrayList<>();
+        try {
+            
+            prep = con.prepareStatement(SELECT_BY_DATE);
+            prep.setDate(1, new java.sql.Date(from.getTime()));
+            prep.setDate(2, new java.sql.Date(to.getTime()));
+            rs = prep.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(4);
+                arr.add("ID: " + id + ": " + name);
+            }
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return arr;
+    }
+    
+    public void insert (Date date, String group, String name, int price) {
+        try {
+            
+            prep = con.prepareStatement(INSERT);
+            prep.setDate(1, new java.sql.Date(date.getTime()));
+            prep.setString(2, group);
+            prep.setString(3, name);
+            prep.setInt(4, price);
+            prep.executeUpdate();
             
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
     
-    public void executeInsert (String group, String name, int price) {
+    public void update (int id, String name, int price) {
         try {
             
-            prep = con.prepareStatement(INSERT);
-            prep.setString(1, group);
-            prep.setString(2, name);
-            prep.setInt(3, price);
+            prep = con.prepareStatement(UPDATE);
+            prep.setString(1, name);
+            prep.setInt(2, price);
+            prep.setInt(3, id);
+            prep.executeUpdate();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void delete (int id) {
+        try {
+            
+            prep = con.prepareStatement(DELETE);
+            prep.setInt(1, id);
             prep.executeUpdate();
             
         } catch (SQLException ex) {
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,34 @@
 package main;
 
 import app.DBConnector;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         DBConnector db = new DBConnector();
-        db.executeInsert("MILK", "KEFIR", 75);
-        db.executeSelect(2);
+        
+//        db.insert(new java.util.Date(),"ALCOHOL", "BEER", 1);
+//        db.insert(new java.util.Date(),"ALCOHOL", "WINE", 1);
+//        db.insert(new java.util.Date(),"ALCOHOL", "CONIAC", 1);
+        
+//        db.select(2);
+//        db.update(3, "RYAZHENKA", 42);
+//        db.delete(3);
+
+        String dt = "2018.06.30";
+        SimpleDateFormat format = new SimpleDateFormat();
+        format.applyPattern("yyyy.MM.dd");
+        Date docDate= format.parse(dt);
+
+        ArrayList<String> arr = new ArrayList();
+        arr = db.selectByDate(docDate, docDate);
+        for (String s: arr) {
+            System.out.println(s);
+        }
         db.close();
     }
     
